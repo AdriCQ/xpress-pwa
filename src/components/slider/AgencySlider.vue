11af9ea452ed6a104639b0625345fb63833c9eb4
AdriCQ <adrian.capote95@zoho.com>
2023-05-30 10:52:56 -0400

    add agent icon

--- src/components/slider/AgencySlider.vue
+++ src/components/slider/AgencySlider.vue
@@ -1,12 +1,16 @@
 <script setup lang='ts'>
+import { defineAsyncComponent } from 'vue';
 
+const AgentIcon = defineAsyncComponent(() => import('@/components/icons/AgentIcon.vue'))
 </script>
 
 <template>
 	<ul class="relative flex overflow-x-scroll overflow-y-hidden space-x-2 p-2 text-md">
 		<li class="flex-shrink-0" v-for="i in 10" :key="`vendor-${i}`">
-			<div class="py-2 px-4 shadow-lg border rounded-xl" :class="i === 1 ? 'bg-slate-700 text-white' : ''"> AG205{{
-				i }}
+			<div class="py-2 px-4 shadow-lg border rounded-xl flex gap-2" :class="i === 1 ? 'bg-slate-700 text-white' : ''">
+				AG205{{
+					i }}
+				<AgentIcon />
 			</div>
 		</li>
 	</ul>
