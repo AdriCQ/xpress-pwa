753d5cf072e7f62641f44469780d245f8e0b2fea
AdriCQ <adrian.capote95@zoho.com>
2023-06-01 13:42:44 -0400

    separated into layouts

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,5 @@
 <script setup lang="ts">
-import { onBeforeMount } from 'vue'
 import { RouterView } from 'vue-router'
-import { getDateYearAndWeek } from '@/helpers'
-import useService from '@/services'
-import { useStore } from '@/stores'
-
-const { getAgencies, getCobranza } = useService()
-const $store = useStore()
-
-/**
- * preloadData
- */
-async function preloadData() {
-  try {
-    const agenciesResp = await getAgencies('Ger003')
-    $store.agencies = agenciesResp.data
-    $store.selectedAgency = $store.agencies[0]
-
-    const { week, year } = getDateYearAndWeek()
-    const cobranzaResp = await getCobranza({
-      agent: $store.selectedAgency,
-      week,
-      year
-    })
-
-    $store.cobranzas = cobranzaResp.data.cobranza
-    console.log({
-      cobranzaResp,
-      agenciesResp
-    })
-  } catch (error) {
-    console.log({ preloadError: error })
-  }
-}
-
-onBeforeMount(async () => {
-  await preloadData()
-})
 </script>
 
 <template>
---
+++ src/components/layouts/DashboardLayout.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { computed, onBeforeMount } from 'vue'
+import { RouterView, useRouter } from 'vue-router'
+import { ROUTE_NAME } from '@/router'
+import useService from '@/services'
+import { useStore } from '@/stores'
+
+const { getAgencies, getCobranza, getCurrentDate, getGerencies } = useService()
+
+const $router = useRouter()
+const $store = useStore()
+
+const currentDate = computed(() => $store.currentDate)
+const user = computed(() => $store.user)
+
+function checkUser() {
+  if (!user.value || !user.value.usuarioId) {
+    void $router.push({ name: ROUTE_NAME.AUTH_LOGIN })
+    return false
+  }
+  return true
+}
+
+/**
+ * preloadData
+ */
+async function preloadData() {
+  if (user.value) {
+    try {
+      const gerenciasResp = await getGerencies(user.value.usuario)
+
+      $store.gerencias = gerenciasResp.data
+      $store.gerenciaSelected = $store.gerencias[0]
+
+      const agenciesResp = await getAgencies($store.gerenciaSelected)
+      $store.agencies = agenciesResp.data
+      $store.agencySelected = $store.agencies[0]
+
+      const cobranzaResp = await getCobranza({
+        agency: $store.agencySelected,
+        week: currentDate.value.week,
+        year: currentDate.value.year
+      })
+
+      $store.cobranzas = cobranzaResp.data.cobranza
+      console.log({
+        cobranzaResp,
+        agenciesResp
+      })
+
+      const dateResp = await getCurrentDate()
+      $store.currentDate = {
+        week: dateResp.data.semana,
+        year: dateResp.data.anio
+      }
+      console.log({ currentDate: $store.currentDate })
+    } catch (error) {
+      console.log({ preloadError: error })
+    }
+  }
+}
+
+onBeforeMount(async () => {
+  if (checkUser()) await preloadData()
+})
+</script>
+
+<template>
+  <RouterView />
+</template>
