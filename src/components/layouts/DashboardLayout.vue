87bb0852ef1b5f38f8a350778990d56c93cb9ca9
AdriCQ <adrian.capote95@zoho.com>
2023-06-01 15:10:42 -0400

    check array index before assign

--- src/components/layouts/DashboardLayout.vue
+++ src/components/layouts/DashboardLayout.vue
@@ -13,56 +13,62 @@ const $store = useStore()
 const user = computed(() => $store.user)
 
 function checkUser() {
-  if (!user.value || !user.value.usuarioId) {
-    void $router.push({ name: ROUTE_NAME.AUTH_LOGIN })
-    return false
-  }
-  return true
+	if (!user.value || !user.value.usuarioId) {
+		void $router.push({ name: ROUTE_NAME.AUTH_LOGIN })
+		return false
+	}
+	return true
 }
 
 /**
  * preloadData
  */
 async function preloadData() {
-  if (user.value) {
-    $store.loading = true
-    try {
-      const gerenciasResp = await getGerencies(user.value.usuario)
+	if (user.value) {
+		$store.loading = true
+		try {
+			const gerenciasResp = await getGerencies(user.value.usuario)
 
-      $store.gerencias = gerenciasResp.data
-      $store.gerenciaSelected = $store.gerencias[0]
+			$store.gerencias = gerenciasResp.data
+			if ($store.gerencias.length) {
+				$store.gerenciaSelected = $store.gerencias[0]
 
-      const agenciesResp = await getAgencies($store.gerenciaSelected)
-      $store.agencies = agenciesResp.data
-      $store.agencySelected = $store.agencies[0]
+				const agenciesResp = await getAgencies($store.gerenciaSelected)
+				$store.agencies = agenciesResp.data
 
-      const dateResp = await getCurrentDate()
-      $store.currentDate = {
-        week: dateResp.data.semana,
-        year: dateResp.data.anio
-      }
-      console.log({ currentDate: $store.currentDate })
+				if ($store.agencies.length) {
+					$store.agencySelected = $store.agencies[0]
 
-      const cobranzaResp = await getCobranza({
-        agency: $store.agencySelected,
-        week: $store.currentDate.week,
-        year: $store.currentDate.year
-      })
+					const dateResp = await getCurrentDate()
+					$store.currentDate = {
+						week: dateResp.data.semana,
+						year: dateResp.data.anio
+					}
+					console.log({ currentDate: $store.currentDate })
 
-      $store.cobranzas = cobranzaResp.data.cobranza
-      $store.cobranzaSelected = $store.cobranzas[0]
-    } catch (error) {
-      console.log({ preloadError: error })
-    }
-    $store.loading = false
-  }
+					const cobranzaResp = await getCobranza({
+						agency: $store.agencySelected,
+						week: $store.currentDate.week,
+						year: $store.currentDate.year
+					})
+
+					$store.cobranzas = cobranzaResp.data.cobranza
+					if ($store.cobranzas.length)
+						$store.cobranzaSelected = $store.cobranzas[0]
+				}
+			}
+		} catch (error) {
+			console.log({ preloadError: error })
+		}
+		$store.loading = false
+	}
 }
 
 onBeforeMount(async () => {
-  if (checkUser()) await preloadData()
+	if (checkUser()) await preloadData()
 })
 </script>
 
 <template>
-  <RouterView />
+	<RouterView />
 </template>
--- src/components/slider/GerencySlider.vue
+++ src/components/slider/GerencySlider.vue
@@ -6,7 +6,7 @@ import useService from '@/services'
 const AgentIcon = defineAsyncComponent(() => import('@/components/icons/AgentIcon.vue'))
 
 defineProps<{
-  gerencias: string[]
+	gerencias: string[]
 }>()
 const $emits = defineEmits<{ (e: 'selectGerency'): void }>()
 const $store = useStore()
@@ -16,44 +16,41 @@ const currentDate = computed(() => $store.currentDate)
 const gerencySeleted = computed(() => $store.gerenciaSelected)
 
 async function selectGerency(gerency: string) {
-  $store.gerenciaSelected = gerency
-  try {
-    $store.loading = true
+	$store.gerenciaSelected = gerency
+	try {
+		$store.loading = true
 
-    const resp = await $service.getAgencies(gerency)
-    $store.agencies = resp.data
-    $store.agencySelected = $store.agencies[0]
-    const cobranzaResp = await $service.getCobranza({
-      agency: $store.agencySelected,
-      week: currentDate.value.week,
-      year: currentDate.value.year
-    })
+		const resp = await $service.getAgencies(gerency)
+		$store.agencies = resp.data
+		if ($store.agencies.length) {
+			$store.agencySelected = $store.agencies[0]
+			const cobranzaResp = await $service.getCobranza({
+				agency: $store.agencySelected,
+				week: currentDate.value.week,
+				year: currentDate.value.year
+			})
 
-    $store.cobranzas = cobranzaResp.data.cobranza
-    $store.cobranzaSelected = $store.cobranzas[0]
+			$store.cobranzas = cobranzaResp.data.cobranza
+			if ($store.cobranzas.length)
+				$store.cobranzaSelected = $store.cobranzas[0]
+		}
+	} catch (error) {
+		console.log({ error })
+	}
+	$store.loading = false
 
-    $store.loading = false
-  } catch (error) {
-    console.log({ error })
-  }
 }
 </script>
 
 <template>
-  <ul class="text-md relative flex space-x-2 overflow-y-hidden overflow-x-scroll p-2">
-    <li
-      class="flex-shrink-0 cursor-pointer"
-      v-for="(gerencia, gerKey) in gerencias"
-      :key="`gerencia-${gerKey}`"
-      @click="() => selectGerency(gerencia)"
-    >
-      <div
-        class="flex gap-2 rounded-xl border px-4 py-2 shadow-lg"
-        :class="gerencia === gerencySeleted ? 'bg-slate-700 text-white' : ''"
-      >
-        {{ gerencia }}
-        <AgentIcon />
-      </div>
-    </li>
-  </ul>
+	<ul class="text-md relative flex space-x-2 overflow-y-hidden overflow-x-scroll p-2">
+		<li class="flex-shrink-0 cursor-pointer" v-for="(gerencia, gerKey) in gerencias" :key="`gerencia-${gerKey}`"
+			@click="() => selectGerency(gerencia)">
+			<div class="flex gap-2 rounded-xl border px-4 py-2 shadow-lg"
+				:class="gerencia === gerencySeleted ? 'bg-slate-700 text-white' : ''">
+				{{ gerencia }}
+				<AgentIcon />
+			</div>
+		</li>
+	</ul>
 </template>
