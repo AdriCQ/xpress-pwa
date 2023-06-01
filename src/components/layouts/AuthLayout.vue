c933080d86519c19bc66490bc3534f7d8bf52520
AdriCQ <adrian.capote95@zoho.com>
2023-06-01 13:42:29 -0400

    create login view

---
+++ src/components/layouts/AuthLayout.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="flex h-screen items-center justify-center">
+    <RouterView />
+  </div>
+</template>
