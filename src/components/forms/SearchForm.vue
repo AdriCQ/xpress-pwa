2d18390a9d6c4f1e515683aa00577ba452409b69
AdriCQ <adrian.capote95@zoho.com>
2023-06-01 22:21:13 -0400

    updated placeholder

--- src/components/forms/SearchForm.vue
+++ src/components/forms/SearchForm.vue
@@ -40,7 +40,7 @@ function onSubmit() {
         id="default-search"
         v-model="search"
         class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-        placeholder="Quick search for anything"
+        placeholder="Búsqueda por nombre o id"
       />
     </div>
   </form>
