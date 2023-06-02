52a0ffb76a935fc6545f167233ca8e381797a086
AdriCQ <adrian.capote95@zoho.com>
2023-06-01 23:54:42 -0400

    add icon to login

--- src/views/AuthLogin.vue
+++ src/views/AuthLogin.vue
@@ -36,16 +36,18 @@ async function onSubmit() {
   <main>
     <section class="bg-gray-50 dark:bg-gray-900">
       <div class="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
-        <!-- <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-																					<img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
-																												Flowbite
-																											</a> -->
+        <a
+          href="#"
+          class="mb-6 flex items-center text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          <img class="mr-2 w-32" src="/icons/logo.png" alt="Xpress" />
+        </a>
         <div
           class="w-full rounded-lg bg-white shadow dark:border dark:border-gray-700 dark:bg-gray-800 sm:max-w-md md:mt-0 xl:p-0"
         >
           <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
             <h1
-              class="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl"
+              class="text-center text-xl leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl"
             >
               Inicie con su cuenta
             </h1>
